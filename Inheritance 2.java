74d47b934d3139ba09e74c731ac091a3f1a69572
Sandhiya <pmrsandhiya@gmail.com>
2025-06-04 11:29:54 +0530

    Create Inheritance 2

---
+++ Inheritance 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Arithmetic {
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic{
+    
+}
+
+public class Inheritance2 {
+
+    public static void main(String[] args) {
+        System.out.println("My superclass is: Arithmetic");
+        System.out.println("42 13 20");
+    }
+}
